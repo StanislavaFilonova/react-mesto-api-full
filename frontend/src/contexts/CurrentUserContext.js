e83d7536f46644989728429221dab8cf0d007a37
Stanislava Filonova <stanislavafilonova@yandex.ru>
2022-07-11 23:55:16 +0300

    Добавляю фронт часть к проекту

---
+++ frontend/src/contexts/CurrentUserContext.js
@@ -0,0 +1,5 @@
+import React from "react";
+// Контекст позволяет передавать данные через дерево компонентов без необходимости передавать пропсы на промежуточных уровнях.
+const CurrentUserContext = React.createContext({});
+
+export default CurrentUserContext;
