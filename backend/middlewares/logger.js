8f037ebbe0dfcacf0a71cc8b2a157cc9818be2f8
Stanislava Filonova <stanislavafilonova@yandex.ru>
2022-07-14 18:23:02 +0300

    Добавила файл .eslintrc к бекэнду

--- backend/middlewares/logger.js
+++ backend/middlewares/logger.js
@@ -1,22 +1,22 @@
 // импортируем нужные модули
-const winston = require("winston");
-const expressWinston = require("express-winston");
+const winston = require('winston');
+const expressWinston = require('express-winston');
 
 // логгер запросов
 const requestLogger = expressWinston.logger({
-    transports: [
-        new winston.transports.File({ filename: "request.log" }), // путь, куда нужно писать лог
-    ],
-    format: winston.format.json(), // формат записи логов
+  transports: [
+    new winston.transports.File({ filename: 'request.log' }), // путь, куда нужно писать лог
+  ],
+  format: winston.format.json(), // формат записи логов
 });
 
 // логгер ошибок
 const errorLogger = expressWinston.errorLogger({
-    transports: [new winston.transports.File({ filename: "error.log" })],
-    format: winston.format.json(),
+  transports: [new winston.transports.File({ filename: 'error.log' })],
+  format: winston.format.json(),
 });
 
 module.exports = {
-    requestLogger,
-    errorLogger,
-  }; 
+  requestLogger,
+  errorLogger,
+};
--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -1,37 +1,36 @@
 // Массив доменов, с которых разрешены кросс-доменные запросы
 const allowedCors = [
-    'https://mesto.filonova.nomorepartiesxyz.ru',
-    'http://mesto.filonova.nomorepartiesxyz.ru',
-    'http://localhost:3001',
-    'http://localhost:3000',
-  ];
-  
+  'https://mesto.filonova.nomorepartiesxyz.ru',
+  'http://mesto.filonova.nomorepartiesxyz.ru',
+  'http://localhost:3001',
+  'http://localhost:3000',
+];
   /* eslint-disable consistent-return */
-  module.exports = (req, res, next) => {
-    // проверяем, что источник запроса есть среди разрешённых 
-    const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-    if (allowedCors.includes(origin)) {
-        // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-      res.header('Access-Control-Allow-Origin', origin);
-    }
-    const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+module.exports = (req, res, next) => {
+  // проверяем, что источник запроса есть среди разрешённых
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  if (allowedCors.includes(origin)) {
+    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
 
-    // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-    const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все запросы)
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
-    // сохраняем список заголовков исходного запроса 
-    const requestHeaders = req.headers['access-control-request-headers'];
-    res.header('Access-Control-Allow-Credentials', true);
+  // сохраняем список заголовков исходного запроса
+  const requestHeaders = req.headers['access-control-request-headers'];
+  res.header('Access-Control-Allow-Credentials', true);
 
-    // Если это предварительный запрос, добавляем нужные заголовки
-    if (method === 'OPTIONS') {
-        // разрешаем кросс-доменные запросы любых типов (по умолчанию) 
-      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-        // разрешаем кросс-доменные запросы с этими заголовками
-      res.header('Access-Control-Allow-Headers', requestHeaders);
-        // завершаем обработку запроса и возвращаем результат клиенту
-      return res.end();
-    }
-  
-    next();
-  };
+  // Если это предварительный запрос, добавляем нужные заголовки
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    // разрешаем кросс-доменные запросы с этими заголовками
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    // завершаем обработку запроса и возвращаем результат клиенту
+    return res.end();
+  }
+
+  next();
+};
