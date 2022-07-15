f26b30a9ef77eddb5c7aa46c8e2de57be344dad9
Stanislava Filonova <stanislavafilonova@yandex.ru>
2022-07-15 10:45:57 +0300

    Изменение логики в файле card.js

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -7,7 +7,7 @@ function Card({ card, onCardClick, onCardLike, onCardDelete }) {
     const currentUser = React.useContext(CurrentUserContext);
 
     // Определяем, являемся ли мы владельцем текущей карточки
-    const isOwn = card.owner._id === currentUser._id;
+    const isOwn = card.owner === currentUser._id;
 
     function handleClick() {
         onCardClick(card);
